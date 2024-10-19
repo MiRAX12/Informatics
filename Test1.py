e6770775fc6483733ab8d521dc3e2a2de6983fed
MiRAX12 <nagorniy.kolya2013@gmail.com>
2024-10-19 20:31:13 +0300

    Add files via upload

---
+++ Test1.py
@@ -0,0 +1,21 @@
+import unittest
+import task1
+#[<{=
+class TestTask1(unittest.TestCase):
+    def test_emojispam(self):
+        data = '[<{=[<{=[<{=[<{=[<{='
+        result = 5
+        self.assertEqual(result, task1.solve(data))
+    def test_wyutnenko(self):
+        data = 'Направо-[<{= Налевобед-8<{'
+        result = 1
+    def test_keyboardface(self):
+        data = '^$%[<{=^&[<{=YUCFG[<{=tyuuiokjfh[[<{=<{=jkg789*I'
+        result = 4
+    def test_Jade_code(self):
+        data = '[习近平<重要贡{=[<方{=方愿[<{=高铁成为双[<{=[<{=地区[<{=[<{=广大发[<{=[<{稳定=[<尼共{='
+        result = 6
+    def test_(self):
+        data = 'Состав: ве[м<{=ль, масло по^&(*ное, вода п(*итьеv[<=aя, !!!соль!!!, витами[>{='
+        result = 0    
+unittest.main()
---
+++ task1.py
@@ -0,0 +1,11 @@
+import re
+#466823 % 6 = 5 => Глаза: [
+#466823 % 4 = 3 => Нос: <{
+#466823 % 8 = 7 => Рот: =
+#Смайлик [<{=
+
+string = input()
+def solve(string):
+    pattern = r'\[<\{='
+    return len(re.findall(pattern, string))
+##print(regex(string))
